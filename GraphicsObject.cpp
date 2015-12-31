eaa47e585229a7a16736866e8b2f7d2b2cde37e2
Chris Butler <chrisbutler296@gmail.com>
2015-12-30 22:33:25 -0700

    Part-way through moderniziing my code!

--- GraphicsObject.cpp
+++ GraphicsObject.cpp
@@ -8,42 +8,10 @@ void GraphicsObject::draw() const {
         return;
     }
 
-    // TODO: glVertexPointer / VBOs.
-    glBegin(GL_TRIANGLES);
-    for (const auto &shape : shapes) {
-        for (const auto &idx : shape.mesh.indices) {
-            int material_id = shape.mesh.material_ids[idx / 3];
-            if (material_id != -1) {
-                // OpenGL expects four floats, but tinyobjloader saves three.
-                glm::vec4 ambient;
-                // My models don't have ambient light. Yay! Just use diffuse.
-                ambient.r = materials[material_id].diffuse[0];
-                ambient.g = materials[material_id].diffuse[1];
-                ambient.b = materials[material_id].diffuse[2];
-                ambient.a = 1.0f;
-                glMaterialfv(GL_FRONT, GL_AMBIENT, glm::value_ptr(ambient));
-
-                glm::vec4 diffuse;
-                diffuse.r = materials[material_id].diffuse[0];
-                diffuse.g = materials[material_id].diffuse[1];
-                diffuse.b = materials[material_id].diffuse[2];
-                diffuse.a = 1.0f;
-                glMaterialfv(GL_FRONT, GL_DIFFUSE, glm::value_ptr(diffuse));
-            }
-
-            if (!shape.mesh.texcoords.empty()) {
-                glTexCoord2f(shape.mesh.texcoords[2 * idx],
-                             shape.mesh.texcoords[2 * idx + 1]);
-            }
-
-            if (!shape.mesh.normals.empty()) {
-                glNormal3fv(&shape.mesh.normals[3 * idx]);
-            }
-
-            glVertex3fv(&shape.mesh.positions[3 * idx]);
-        }
+    for (auto pair : vao_pairs) {
+        glBindVertexArray(pair.first);
+        glDrawElements(GL_TRIANGLES, pair.second, GL_UNSIGNED_INT, nullptr);
     }
-    glEnd();
 }
 
 void GraphicsObject::loadObjFile(const std::string &filename) {
@@ -67,8 +35,10 @@ void GraphicsObject::loadObjFile(const std::string &filename) {
         abort();
     }
 
+    glChk();
     size_t verts = 0;
     for (const auto &shape : shapes) {
+        // Sanity checks.
         assert(shape.mesh.indices.size() % 3 == 0);   // Rendered Triangles
         assert(shape.mesh.positions.size() % 3 == 0); // Unique Triangles
 
@@ -85,8 +55,56 @@ void GraphicsObject::loadObjFile(const std::string &filename) {
                   << std::setw(20) << shape.name            //
                   << std::setw(10) << std::right            //
                   << (shape.mesh.indices.size() / 3) << " vertices\n";
-        verts += shape.mesh.indices.size();
+
+        verts += indices;
+
+        GLuint vao;
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+        glChk();
+
+        // Indices
+        GLuint  ibo;
+        GLsizei count = as<GLsizei>(indices);
+        glGenBuffers(1, &ibo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                     sizeof(shape.mesh.indices[0]) * count,
+                     &shape.mesh.indices[0],
+                     GL_STATIC_DRAW);
+        glChk();
+
+        // Vertices
+        GLuint vbo;
+        glGenBuffers(1, &vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER,
+                     sizeof(shape.mesh.positions[0]) * positions,
+                     &shape.mesh.positions[0],
+                     GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glChk();
+
+        // Normals
+        GLuint normals;
+        GLint normalIdx = glGetAttribLocation(0, // TODO: load a program!
+                                             "gl_Normal");
+        glChk();
+
+        glGenBuffers(1, &normals);
+        glBindBuffer(GL_ARRAY_BUFFER, normals);
+        glBufferData(GL_ARRAY_BUFFER,
+                     sizeof(shape.mesh.normals[0]) * positions,
+                     &shape.mesh.normals[0],
+                     GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(normalIdx, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glChk();
+
+        vao_pairs.emplace_back(vao, count);
     }
+
     std::cout << "\t" << std::string(39, '=') << "\n"                   //
               << "\t" << std::setw(30) << verts << " vertices\n"        //
               << "\t" << std::setw(30) << (verts / 3) << " triangles\n" //
--- GraphicsObject.hpp
+++ GraphicsObject.hpp
@@ -10,6 +10,8 @@ struct GraphicsObject {
     std::vector<tinyobj::shape_t>    shapes;
     std::vector<tinyobj::material_t> materials;
 
+    std::vector<std::pair<GLuint, GLsizei>> vao_pairs;
+
     void draw() const;
 
     void loadObjFile(const std::string &filename);
